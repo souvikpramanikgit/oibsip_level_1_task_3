2c32856c11b88094dc18015735dee5b2ff3102bd
Souvik Kumar Pramanik <123581988+souvikpramanikgit@users.noreply.github.com>
2023-07-20 23:54:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,21 +11,27 @@ btn.addEventListener("click",()=>{
     if (input === "celcius" && output === "fahrenheit") {
         result = val + " °C = " + ((val * (9 / 5)) + 32) + " °F";
     }
+        
     else if (input === "celcius" && output === "kelvin") {
         result = val + " °C = " + (val + 273.15) + " k";
     }
+        
     else if (input === "fahrenheit" && output === "celcius") {
         result = val + " °F = " + ((val - 32) * (5/9)) + " °C";
     }
+        
     else if (input === "fahrenheit" && output === "kelvin") {
         result = val + " °F = " + ((val - 32) * (5/9) + 273.15) + " k";
     }
+        
     else if (input === "kelvin" && output === "celcius") {
         result = val + " k = " + (val - 273.15) + " °C";
     }
+        
     else if (input === "kelvin" && output === "fahrenheit") {
         result = val + " k = " + (((val - 273.15) * (9/5)) + 32) + " °F";
     }
+        
     else{
         result = "|| Same unit ||";
     }
